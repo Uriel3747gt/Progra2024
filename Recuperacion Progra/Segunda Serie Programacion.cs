259948b5b949d45500d433a9a48b57690a365fed
Uriel3747gt <101778681+Uriel3747gt@users.noreply.github.com>
2024-06-22 08:40:07 -0600

    Add files via upload

---
+++ Recuperacion Progra/Segunda Serie Programacion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese el primer número:");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+
+        double suma = Sumar(numero1, numero2);
+        double resta = Restar(numero1, numero2);
+        double multiplicacion = Multiplicar(numero1,numero2);
+        double division = Dividir(numero1, numero2);
+
+        Console.WriteLine("Suma: " + suma);
+        Console.WriteLine("Resta: " + resta);
+        Console.WriteLine("Multiplicación: " + multiplicacion);
+        Console.WriteLine("División: " + division);
+    }
+
+    static double Sumar(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    static double Restar(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    static double Multiplicar(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    static double Dividir(double num1, double num2)
+    {
+        if (num2 == 0)
+        {
+            Console.WriteLine("Error: No se puede dividir por cero.");
+            return 0;
+        }
+        return num1 / num2;
+    }
+}
